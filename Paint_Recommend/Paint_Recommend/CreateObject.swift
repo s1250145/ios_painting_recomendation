3d930b75251db34b3b3dcb332ab04217589d3869
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-09-15 13:30:31 +0900

    絵画詳細画面に詳細情報のラベルを追加

---
+++ Paint_Recommend/Paint_Recommend/CreateObject.swift
@@ -0,0 +1,20 @@
+//
+//  CreateObject.swift
+//  Paint_Recommend
+//
+//  Created by 山口瑞歩 on 2020/09/15.
+//  Copyright © 2020 山口瑞歩. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateObject {
+    static func createLabel(title: String, size: CGFloat) -> UILabel {
+        let label = UILabel(frame: .zero)
+        label.text = title
+        label.font = UIFont.systemFont(ofSize: size)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+}
--- Paint_Recommend/Paint_Recommend/PaintDetailViewController.swift
+++ Paint_Recommend/Paint_Recommend/PaintDetailViewController.swift
@@ -12,21 +12,48 @@ class PaintDetailViewController: UIViewController {
 
     var paint = UIImageView(frame: .zero)
 
+    var name: String = "Unknown"
+    var date: String = "2021"
+    var artist: String = "mixufo"
+    var born: String = "Japan"
+    var active: String = "1998-2028"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        self.title = "ArtTitle"
+        self.title = name
         self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.init(name: "CourierNewPSMT", size: 30) as Any]
 
         paint.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(paint)
 
+        let artTitle = CreateObject.createLabel(title: name, size: 36)
+        view.addSubview(artTitle)
+
+        let artDate = CreateObject.createLabel(title: date, size: 24)
+        view.addSubview(artDate)
+
+        let artistName = CreateObject.createLabel(title: artist, size: 28)
+        view.addSubview(artistName)
+
+        let artistBorn = CreateObject.createLabel(title: born+", "+active, size: 20)
+        view.addSubview(artistBorn)
+
         NSLayoutConstraint.activate([
             paint.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             paint.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             paint.heightAnchor.constraint(equalToConstant: 400),
-            paint.widthAnchor.constraint(equalToConstant: view.frame.width*0.95)
+            paint.widthAnchor.constraint(equalToConstant: view.frame.width*0.95),
+            artTitle.topAnchor.constraint(equalTo: paint.bottomAnchor, constant: 40),
+            artTitle.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            artDate.topAnchor.constraint(equalTo: artTitle.bottomAnchor, constant: 0),
+            artDate.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+
+            artistName.topAnchor.constraint(equalTo: artDate.bottomAnchor, constant: 20),
+            artistName.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            artistBorn.topAnchor.constraint(equalTo: artistName.bottomAnchor, constant: 0),
+            artistBorn.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20)
             ])
     }
 }
