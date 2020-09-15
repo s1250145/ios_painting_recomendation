fecd81283a9d8d03a230e044d117f4b1baa55e6d
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-09-15 11:12:07 +0900

    SBの削除と初期設定

---
+++ Paint_Recommend/Paint_Recommend/PaintCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PaintCollectionViewController.swift
+//  Paint_Recommend
+//
+//  Created by 山口瑞歩 on 2020/09/15.
+//  Copyright © 2020 山口瑞歩. All rights reserved.
+//
+
+import UIKit
+
+class PaintCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
