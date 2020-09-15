8e18286a7bd7dd8972c2a73f67a6b113ebdbb88f
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-09-15 12:23:31 +0900

    絵画詳細画面（VC）の追加

---
+++ Paint_Recommend/Paint_Recommend/PaintDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PaintDetailViewController.swift
+//  Paint_Recommend
+//
+//  Created by 山口瑞歩 on 2020/09/15.
+//  Copyright © 2020 山口瑞歩. All rights reserved.
+//
+
+import UIKit
+
+class PaintDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
