1f0db7ed754ee47eef0fca0b9da63a2a86ff1b2c
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-09-15 11:00:09 +0900

    Xcodeプロジェクトの作成

---
+++ Paint_Recommend/Paint_RecommendTests/Paint_RecommendTests.swift
@@ -0,0 +1,34 @@
+//
+//  Paint_RecommendTests.swift
+//  Paint_RecommendTests
+//
+//  Created by 山口瑞歩 on 2020/09/15.
+//  Copyright © 2020 山口瑞歩. All rights reserved.
+//
+
+import XCTest
+@testable import Paint_Recommend
+
+class Paint_RecommendTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
