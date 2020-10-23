87e30bda669590242adbc0e797e547e8a141999e
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-10-23 15:51:50 +0900

    SprashViewを追加

---
+++ Paint_Recommend/Paint_Recommend/SprashView.swift
@@ -0,0 +1,13 @@
+//
+//  SprashView.swift
+//  Paint_Recommend
+//
+//  Created by 山口瑞歩 on 2020/10/23.
+//  Copyright © 2020 山口瑞歩. All rights reserved.
+//
+
+import UIKit
+
+class SprashView: UIView {
+
+}
