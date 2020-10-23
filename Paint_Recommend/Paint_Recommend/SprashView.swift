2b6eb30a32ed6b47508ab401869d1520b9fdf738
Mizuho Yamaguchi <s1250145u@gmail.com>
2020-10-23 15:55:40 +0900

    SprashViewを作成

--- Paint_Recommend/Paint_Recommend/SprashView.swift
+++ Paint_Recommend/Paint_Recommend/SprashView.swift
@@ -9,5 +9,30 @@
 import UIKit
 
 class SprashView: UIView {
+    let logoImageView = UIImageView(frame: .zero)
 
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        self.layer.backgroundColor = UIColor(red: 187/256, green: 188/256, blue: 222/256, alpha: 1.0).cgColor
+        logoImageView.image = UIImage(named: "Museum")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(logoImageView)
+
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 250),
+            logoImageView.heightAnchor.constraint(equalToConstant: 250)
+            ])
+    }
+}
+
+extension UIView {
+    func addConstraints(for childView: UIView, insets: UIEdgeInsets = .zero) {
+        childView.translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: childView.topAnchor, constant: insets.top).isActive = true
+        bottomAnchor.constraint(equalTo: childView.bottomAnchor, constant: insets.bottom).isActive = true
+        leadingAnchor.constraint(equalTo: childView.leadingAnchor, constant: insets.left).isActive = true
+        trailingAnchor.constraint(equalTo: childView.trailingAnchor, constant: insets.right).isActive = true
+    }
 }
